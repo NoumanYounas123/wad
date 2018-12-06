1b449fd89e8f41b5070fff37694d1f1c518c62bd
NoumanYounas123 <44551430+NoumanYounas123@users.noreply.github.com>
2018-12-06 01:44:50 -0800

    MId MOK test

---
+++ main.js
@@ -0,0 +1,21 @@
+
+var Info = {
+
+title:"Nouman Younas",
+Balance:900000,
+Curruncy:"Dollars",
+IBAN:935345
+};
+  function  Display()
+  {
+	document.getElementById("title").innerHTML=Info.title;
+	document.getElementById("balance").innerHTML=Info.Balance;
+	document.getElementById("currency").innerHTML=Info.Curruncy;
+	document.getElementById("IBAN").innerHTML=Info.IBAN;
+}
+Display();
+function Deposit()
+{
+  var value=getElementById("withdraw").valueof;
+  	if( )
+}
